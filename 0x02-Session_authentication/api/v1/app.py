eb3fa0a7c7e26a853e588bb59732f7983253b86b
phemsie <femi_oworu@yahoo.com>
2023-08-10 23:06:22 +0000

    Session_authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,37 +15,21 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
-if getenv('AUTH_TYPE') == 'auth':
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE:
     from api.v1.auth.auth import Auth
-    auth = Auth()
-if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-if getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-
-
-@app.before_request
-def before_request():
-    """filtering each request
-    """
-    lst = ['/api/v1/status/',
-           '/api/v1/unauthorized/',
-           '/api/v1/forbidden/',
-           '/api/v1/auth_session/login/']
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, lst):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.authorization_header(
-            request) and auth.session_cookie(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    request.current_user = auth.current_user(request)
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth_types = {
+        'auth': Auth,
+        'basic_auth': BasicAuth,
+        'session_auth': SessionAuth,
+        'session_exp_auth': SessionExpAuth,
+        'session_db_auth': SessionDBAuth
+    }
+    auth = auth_types[AUTH_TYPE]()
 
 
 @app.errorhandler(404)
@@ -57,16 +41,36 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
-    """request not authorized handler
+    """ Not authorized error
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
-    """when user is forbidden
-    """
+def forbidden(error) -> str:
+    """ Forbid user from access even if user is authenticated. """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def b4_each_request() -> None:
+    """ Runs before every request """
+    if auth is None:
+        return None
+    excluded_paths = [
+        '/api/v1/stat*',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return None
+    if auth.authorization_header(request) is None and \
+       auth.session_cookie(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
