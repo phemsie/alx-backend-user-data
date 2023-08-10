10cd739c56b4a2f1f70c11cd0f5ca5bc6d697679
phemsie <femi_oworu@yahoo.com>
2023-08-10 23:59:41 +0000

    basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
-""" module of api authentication
-"""
+""" Base class for all auth instances """
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
-    """ manages API authentication
-    """
+class Auth:
+    """ Class Auth """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False
-        """
-        if path is None:
+        """ Returns true if a path is not among excluded paths """
+        if path is None or excluded_paths is None:
             return True
-        if excluded_paths is None or excluded_paths == []:
+
+        stripped_routes_list = list(map(lambda x: x.rstrip('/'),
+                                        excluded_paths))
+        # - Implement wildcard matching -
+        for p in stripped_routes_list:
+            if p.endswith('*'):
+                if path.startswith(p.rstrip('*')):
+                    return False
+        # - ----------- END ----------- -
+
+        if path.rstrip('/') not in stripped_routes_list:
             return True
-        if path[-1] != '/':
-            path = path + '/'
-        if path in excluded_paths:
-            return False
-        return True
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """ returns None,
-        request will be the Flask request object
-        """
-        if request is None:
-            return None
-        if request.headers.get('Authorization') is None:
-            return None
-        return request.headers.get('Authorization')
+        """ Retreives authorization header if available """
+        if request:
+            if request.headers.get('Authorization'):
+                return request.headers.get('Authorization')
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ returns None,
-        request will be the Flask request object
-        """
+        """ Does nothing """
         return None
