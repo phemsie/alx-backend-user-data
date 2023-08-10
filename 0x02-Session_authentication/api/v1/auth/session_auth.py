b905d8cf45e7c47fa8aeaf31721ff74e1a8166ab
phemsie <femi_oworu@yahoo.com>
2023-08-10 23:22:40 +0000

    Session_authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,41 +1,49 @@
 #!/usr/bin/env python3
-""" SessionAuth inherits from Auth
 """
-import uuid
-from api.v1.auth.auth import Auth
+Session authentication module that contains a class `SessionAuth` that
+extends `api.v1.auth.auth.Auth`
+"""
+from .auth import Auth
+from typing import TypeVar
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """ implements SessionAuth authentication mechanism
-    """
+    """ A class that implements session authentication """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ creates a session id for a user_id
-        """
-        if user_id is None:
-            return None
-        if not isinstance(user_id, str):
-            return None
-        sessionID = str(uuid.uuid4())
-        self.user_id_by_session_id[sessionID] = user_id
-        return sessionID
+        """ A method that creates a session id for a user_id """
+        if user_id and type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns a User ID based on a Session ID
-        """
-        if session_id is None:
-            return None
-        if not isinstance(session_id, str):
-            return None
-        return str(self.user_id_by_session_id.get(session_id))
+        """ A method that returns user id based on a session id """
+        if session_id and type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+        return None
 
     def current_user(self, request=None):
-        """ Returns a User instance based on a cookie value
-        """
-        user_session = self.session_cookie(request)
-        if user_session is None:
-            return None
-        user_id = self.user_id_for_session_id(user_session)
-        return User.get(user_id)
+        """ A method that overloads and returns a user instance based on a
+        cookie value """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                user_id = self.user_id_for_session_id(session_id)
+                if user_id:
+                    user = User.get(user_id)
+                    return user
+        return None
+
+    def destroy_session(self, request=None):
+        """ A method that deletes the user session (logout function) """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id and self.user_id_for_session_id(session_id):
+                del self.user_id_by_session_id[session_id]
+                return True
+        return False
